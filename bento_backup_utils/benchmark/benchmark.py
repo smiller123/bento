74abe6880ce9ff1412aa59e009b0e3d3cafd44e0
Teerapat Jenrungrot <15001582+mjenrungrot@users.noreply.github.com>
2020-11-21 10:55:48 +0700

    Add parameters for benchmark

--- bento_backup_utils/benchmark/benchmark.py
+++ bento_backup_utils/benchmark/benchmark.py
@@ -1,3 +1,5 @@
+# TODO: Order import by PEP8 standards
+import argparse
 import randomfiletree
 import pathlib
 import shutil
@@ -8,33 +10,32 @@ import time
 import subprocess
 from typing import Callable
 
-TEST_DIR = "test_dir"
-SRC_DIR = "test_dir/src_dir"
-DEST_DIR = "test_dir/dest_dir"
+SRC_DIR = "src_dir"
+DEST_DIR = "dest_dir"
 
-def create_test_dir(n_files: int,
+def create_test_dir(src_dir: str,
+                    n_files: int,
                     n_folders: int,
                     max_depth: int,
-                    repeat: int, 
+                    repeat: int,
                     payload: Callable) -> None:
    """
    Create a test dir.
    """
-
    randomfiletree.iterative_gaussian_tree(
-      SRC_DIR,
+      src_dir,
       nfiles=n_files,
       nfolders=n_folders,
       maxdepth=max_depth,
       repeat=repeat,
-      payload=callback
+      payload=callback,
    )
 
-def remove_test_dir():
+def remove_test_dir(path: str) -> None:
    """
    Remove a test dir.
    """
-   shutil.rmtree(TEST_DIR)
+   shutil.rmtree(path)
 
 def callback(target_dir: pathlib.Path) -> pathlib.Path:
    """
@@ -47,7 +48,7 @@ def callback(target_dir: pathlib.Path) -> pathlib.Path:
          f.write('aaaa')
       yield path
 
-def iterate_files(action: Callable, prob: float):
+def iterate_files(action: Callable, src_dir: str, prob: float) -> None:
    """
    iterate over the files in SRC_DIR and perform action
    """
@@ -55,20 +56,20 @@ def iterate_files(action: Callable, prob: float):
       raise ValueError("prob must be >= 0 and <=1")
 
    # TODO: change to pathlib
-   for directory, subdirs, files in os.walk(SRC_DIR):
+   for directory, subdirs, files in os.walk(src_dir):
       for filename in files:
          r = random.random()
          if r < prob:
             action(pathlib.Path(directory), filename)
 
-def remove_file(directory: pathlib.Path, filename: str):
+def remove_file(directory: pathlib.Path, filename: str) -> None:
    """
    remove filename in directory
    """
    path = directory / filename
    os.remove(path)
 
-def modify_file(directory: pathlib.Path, filename: str):
+def modify_file(directory: pathlib.Path, filename: str) -> None:
    """
    modify filename in directory
    """
@@ -76,7 +77,7 @@ def modify_file(directory: pathlib.Path, filename: str):
    with open(path, 'a') as f:
       f.write('b')
 
-def rename_file(directory: pathlib.Path, filename: str):
+def rename_file(directory: pathlib.Path, filename: str) -> None:
    """
    rename filename in directory to a new random filename
    """
@@ -84,14 +85,14 @@ def rename_file(directory: pathlib.Path, filename: str):
    dest = directory / randomfiletree.core.random_string()
    os.rename(src, dest)
 
-def iterate_directories(action, prob):
+def iterate_directories(action: Callable, src_dir: str, prob: float) -> None:
    """
    iterate over subdir in SRC_DIR and perform action
    """
    if not (0 <= prob <= 1):
       raise ValueError("prob must be >= 0 and <=1")
 
-   for directory, subdirs, files in os.walk(SRC_DIR):
+   for directory, subdirs, files in os.walk(src_dir):
       for subdir in subdirs:
          r = random.random()
          if r < prob:
@@ -111,36 +112,122 @@ def rename_dir(directory: pathlib.Path, subdir: str):
    dest = directory / randomfiletree.core.random_string()
    os.rename(src, dest)
 
-def run_rsync():
+def run_rsync(src_path: str, dest_path: str) -> None:
    """
-   run rsync on the SRC_DIR
+   run rsync on the source directory
    """
    start_time = time.time()
-   subprocess.call(['rsync', '-r', SRC_DIR + "/", DEST_DIR])
+   subprocess.call(['rsync', '-r', src_path + "/", dest_path])
    end_time = time.time()
    duration = end_time - start_time
    print('rsync: {} s'.format(duration))
 
-def main():
-   if os.path.isdir(TEST_DIR):
-      remove_test_dir()
+def run_bento(src_path: str, dest_path: str) -> None:
+   start_time = time.time()
+   # TODO: get a list of files to be updated / removed
 
-   create_test_dir(
-      n_files=5,
-      n_folders=5,
-      max_depth = 5,
-      repeat=3,
-      payload=callback)
+   # TODO: filter the list based on src_path and dest_path
 
-   iterate_files(modify_file, 0.5)
-   iterate_files(remove_file, 0.3)
-   iterate_files(rename_file, 0.2)
+   # TODO: run fs copy
 
-   iterate_directories(remove_dir, 0.1)
-   iterate_directories(rename_dir, 0.1)
+   end_time = time.time()
+   duration = end_time - start_time
+   print("bento: {} s".format(duration))
 
-   run_rsync()
-   remove_test_dir()
+def main(args: argparse.Namespace) -> None:
+   # Remove target directory if it exists beforehand
+   if os.path.isdir(args.src_path):
+      remove_test_dir(args.src_path)
+
+   if os.path.isdir(args.dest_path):
+      remove_test_dir(args.dest_path)
+
+   # Create a random directory tree
+   create_test_dir(args.src_path,
+                   n_files=args.n_files,
+                   n_folders=args.n_dirs,
+                   max_depth=args.max_depth,
+                   repeat=args.max_depth,
+                   payload=callback)
+
+   # TODO: copy src_path to dest_path
+
+   # Make modifications on src_path
+   iterate_files(modify_file, args.src_path, args.modfile_prob)
+   iterate_files(remove_file, args.src_path, args.rmfile_prob)
+   iterate_files(rename_file, args.src_path, args.renamefile_prob)
+   iterate_directories(remove_dir, args.src_path, args.rmdir_prob)
+   iterate_directories(rename_dir, args.src_path, args.renamedir_prob)
+
+   # Run benchmark
+   if args.mode == 'rsync':
+      run_rsync(args.src_path, args.dest_path)
+   elif args.mode == 'bento':
+      run_bento(args.src_path, args.dest_path)
+   else:
+      raise NotImplementedError("mode not yet supported")
+
+   if not args.skip_cleanup:
+      remove_test_dir(args.src_path)
+      remove_test_dir(args.dest_path)
 
 if __name__ == "__main__":
-   main()
+   parser = argparse.ArgumentParser()
+   parser.add_argument('--src-path',
+                       type=str,
+                       default=SRC_DIR,
+                       help="Source directory")
+   parser.add_argument('--dest-path',
+                       type=str,
+                       default=DEST_DIR,
+                       help="Destination directory")
+   parser.add_argument('--mode',
+                       type=str,
+                       default='rsync',
+                       help="'rsync' or 'bento'")
+
+   # Directory tree parameters
+   parser.add_argument('--n-files',
+                       type=int,
+                       default=5,
+                       help="")
+   parser.add_argument('--n-dirs',
+                       type=int,
+                       default=5,
+                       help="")
+   parser.add_argument('--max-depth',
+                       type=int,
+                       default=5,
+                       help="")
+   parser.add_argument('--repeat',
+                       type=int,
+                       default=3,
+                       help="")
+
+   # File/directory modification parameters
+   parser.add_argument('--modfile-prob',
+                       type=float,
+                       default=0.5,
+                       help="")
+   parser.add_argument('--rmfile-prob',
+                       type=float,
+                       default=0.3,
+                       help="")
+   parser.add_argument('--renamefile-prob',
+                       type=float,
+                       default=0.3,
+                       help="")
+   parser.add_argument('--rmdir-prob',
+                       type=float,
+                       default=0.3,
+                       help="")
+   parser.add_argument('--renamedir-prob',
+                       type=float,
+                       default=0.3,
+                       help="")
+   parser.add_argument('--skip-cleanup',
+                       type=bool,
+                       default=False,
+                       help="Whether to remove tmp directory tree after benchmark")
+   args = parser.parse_args()
+   main(args)
