a51ef53cb0baecf952e624ff7bb16d540e3d0306
Nicolas Monsees <nmonsees@cs.washington.edu>
2020-11-23 17:14:07 -0800

    reading files to update from the .lin, initializing inode map with mount point, fixed files_to_update for mkdir

--- bento_backup_utils/src/main.rs
+++ bento_backup_utils/src/main.rs
@@ -8,19 +8,36 @@ mod parser;
 mod fs;
 
 fn run_utility(mount_point: &Path, remote: &Path, scan_frequency: &u64) {
-    let inode_map = HashMap::<u64, PathBuf>::new();
-    // assert!(mount_point.is_dir());
-    // assert!(remote.is_dir());
+    assert!(mount_point.is_dir());
+    assert!(remote.is_dir());
+
+    let mut inode_map = HashMap::<u64, PathBuf>::new();
+
+    // add root to map, since its creation isn't logged
+    inode_map.insert(1, PathBuf::from(mount_point));
+
     let lin_file = mount_point.join(".lin");
     loop {
         println!("in loop!");
-        // println!("mount_point to_str {:?}", mount_point.to_str().unwrap());
         let mut events = Vec::<parser::Event>::new();
         assert!(events.len() == 0);
 
         // TODO(nmonsees): eventually this should just return the subset based on prev size
         let lin_contents = parser::read_lin_file(lin_file.to_str().unwrap()).expect("Unable to read lin file from mount point");
-        
+        parser::populate_events(&mut events, lin_contents);  // TODO(nmonsees): will print error, need to change in parser
+
+        //events.iter().for_each(|e| { println!("event! {:?}", e) });
+
+        parser::update_inode_map(&mut inode_map, &events);
+        for (key, value) in &inode_map {
+            println!("{}: {}", key, value.display());
+        }
+
+        let files = parser::files_to_update(&inode_map, &events);
+        for (key, value) in &files {
+            println!("{}: {:?}", key.to_str().unwrap(), value);
+        }
+
         thread::sleep(time::Duration::from_millis(*scan_frequency));
     }
 }
